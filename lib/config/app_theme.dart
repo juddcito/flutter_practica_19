727763861adcabc0c7054b6d9fd87054dd528da1
Juan Sapién <107738378+juddcito@users.noreply.github.com>
2023-11-11 16:45:57 -0700

    cambié el color del tema

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -6,7 +6,7 @@ class AppTheme{
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: Colors.deepOrangeAccent,
+    colorSchemeSeed: Colors.blueAccent,
     brightness: Brightness.light
   );
 
